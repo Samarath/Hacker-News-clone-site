57f833f8bafe3cc32abee423d1fde70ed8250bcd
Samrath <samrathkumarmuz@gmail.com>
2020-06-01 08:32:29 +0530

    Updates

---
+++ precache-manifest.aa17ea04c9d3e48a240a00cb69481f35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3b3d9333d4e42277dc35396db78c8f",
+    "url": "/Samarath/Hacker-News-clone-site/index.html"
+  },
+  {
+    "revision": "c5e21e99028ac8458626",
+    "url": "/Samarath/Hacker-News-clone-site/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e3f9e8a32107f8e784ed",
+    "url": "/Samarath/Hacker-News-clone-site/static/css/main.0219bcb7.chunk.css"
+  },
+  {
+    "revision": "c5e21e99028ac8458626",
+    "url": "/Samarath/Hacker-News-clone-site/static/js/2.b7c91fb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Samarath/Hacker-News-clone-site/static/js/2.b7c91fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f9e8a32107f8e784ed",
+    "url": "/Samarath/Hacker-News-clone-site/static/js/main.fa671902.chunk.js"
+  },
+  {
+    "revision": "16f3ba65ea4429b3a385",
+    "url": "/Samarath/Hacker-News-clone-site/static/js/runtime-main.c4373b3b.js"
+  }
+]);
